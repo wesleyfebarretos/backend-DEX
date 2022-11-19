9fa7d44216e768652b10e3d3210cb9feecfddf95
Wesley Ferreira <wesleyfebarretos@gmail.com>
2022-11-19 20:20:19 -0300

    controller function cal fixed

--- src/services/pokemon-service/pokemon-controller.ts
+++ src/services/pokemon-service/pokemon-controller.ts
@@ -7,24 +7,24 @@ export class PokemonController {
     this.pokemonService = pokemonService;
   }
 
-  getAll = async (req: Request, res: Response) => {
+  async getAll(req: Request, res: Response) {
     const offset = Number(req.query.offset) || 0;
     const limit = Number(req.query.limit) || 24;
 
     const result = await this.pokemonService.getAll(offset, limit);
 
     res.json(result);
-  };
+  }
 
-  getOne = async (req: Request, res: Response) => {
+  async getOne(req: Request, res: Response) {
     const { pokemon } = req.params;
 
     const result = await this.pokemonService.getOne(pokemon);
 
     res.json(result);
-  };
+  }
 
-  create = async (req: Request, res: Response) => {
+  async create(req: Request, res: Response) {
     const { name, abilities, types, sprites } = req.body;
 
     const result = await this.pokemonService.create(
@@ -38,49 +38,49 @@ export class PokemonController {
       id: result,
       message: `New pokemon ${name} created successfully`,
     });
-  };
+  }
 
-  updateName = async (req: Request, res: Response) => {
+  async updateName(req: Request, res: Response) {
     const { name } = req.body;
     const { pokemon } = req.params;
 
     await this.pokemonService.updateName(name, pokemon);
 
     res.json({ message: `Pokemon ${pokemon} successfully updated` });
-  };
+  }
 
-  updateAbilities = async (req: Request, res: Response) => {
+  async updateAbilities(req: Request, res: Response) {
     const { abilities } = req.body;
     const { pokemon } = req.params;
 
     await this.pokemonService.updateAbilities(abilities, pokemon);
 
     res.json({ message: `Pokemon ${pokemon} successfully updated` });
-  };
+  }
 
-  updateTypes = async (req: Request, res: Response) => {
+  async updateTypes(req: Request, res: Response) {
     const { types } = req.body;
     const { pokemon } = req.params;
 
     await this.pokemonService.updateTypes(types, pokemon);
 
     res.json({ message: `Pokemon ${pokemon} successfully updated` });
-  };
+  }
 
-  updateSprites = async (req: Request, res: Response) => {
+  async updateSprites(req: Request, res: Response) {
     const { sprites } = req.body;
     const { pokemon } = req.params;
 
     await this.pokemonService.updateSprites(sprites, pokemon);
 
     res.json({ message: `Pokemon ${pokemon} successfully updated` });
-  };
+  }
 
-  delete = async (req: Request, res: Response) => {
+  async delete(req: Request, res: Response) {
     const { pokemon } = req.params;
 
     await this.pokemonService.delete(pokemon);
 
     res.status(StatusCodes.NO_CONTENT).send();
-  };
+  }
 }
